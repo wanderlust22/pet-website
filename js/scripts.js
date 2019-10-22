95f69a3668ba2c221e28de625cd3c9406f8e54cb
Eddie Raskin and Will Quanstrom <eddie@>
2019-10-22 09:47:46 -0700

    add buttons to switch page themes

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// $(document).ready(function()  {
+//   $("button#dark").click(function() {
+//     $("body").addClass("dark1");
+//   });
+// });
+
+$(document).ready(function(){
+
+  $("button#dark").click(function() {
+      $("body").removeClass();
+      $("body").addClass("dark1");
+    });
+
+  $("button#light").click(function()  {
+    $("body").removeClass();
+    $("body").addClass("light1");
+  });
+  });
